285b40d84136f70f5fa7b9cce28acfb50097a5ca
Xhostcom <xhostcom@gmail.com>
2021-03-22 06:05:24 +0000

    Gitacora saved 1 changed files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.hosts << "main-friends-wr7le49y8z1g6pul-gtw.qovery.io"
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
