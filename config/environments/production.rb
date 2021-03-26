3812c36e2d485196ea878268849cd3bc4cbf6d7c
Paul Anthony McGowan <xhostcom@gmail.com>
2021-03-26 11:24:11 +0000

    Gitacora saved 3 changed files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.hosts = "0.0.0.0"
+  config.hosts << "127.0.0.1"
 
 
   # Compress CSS using a preprocessor.
